bf23cca4c9913c380c37154856f196f8d21b67ad
essleeung <essleeung@gmail.com>
2020-05-01 18:07:08 -0400

    finished p2 assessment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const methodOverride = require('method-override');
+const db = require('./models')
 
 const app = express();
 
@@ -11,6 +12,40 @@ app.use(methodOverride('_method'));
 // WRITE YOUR ROUTES HERE /////////////////////
 
 
+//GET- root route
+app.get('/', (req, res) => {
+    db.widget.findAll()
+    .then(widgets => {
+        res.render('index', {widgets})
+    })
+    .catch(err => 
+        console.log("ERROR!", err)
+    )}
+)
+//POST- add a widget
+app.post('/', (req, res) => {
+    db.widget.create(req.body)
+    .then(widget => {
+        console.log("success! created: ", widget)
+        res.redirect('/')
+    }).catch(err => {
+        console.log("ERROR!", err )
+    })
+})
+
+//DELETE- delete a widget
+app.delete('/:id', (req, res) => {
+    db.widget.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(() => {
+        res.redirect('/')
+    })
+    .catch(err => {
+        console.log("ERROR!", err )
+    })    
+})
 
 // YOUR ROUTES ABOVE THIS COMMENT /////////////
 
